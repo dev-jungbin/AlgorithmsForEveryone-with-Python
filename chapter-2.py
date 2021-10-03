f3ded0e840c37020fce0dea069da1b488ca212d9
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2021-10-04 00:36:16 +0900

    Solve chapter 2

--- chapter-2.py
+++ chapter-2.py
@@ -1,8 +1,29 @@
-def find_max(numList):
+# 2-0
+def c2_0(numList):
     max = numList[0]
     for i in range(0, len(numList)):
         if max < numList[i]:
             max = numList[i]
     return max
+
+# 2-0-1
+def c2_0_1(numList):
+    maxIdx = 0
+    temp = numList[0]
+    for i in range(0, len(numList)):
+        if temp < numList[i]:
+            temp = numList[i]
+            maxIdx = i
+    return maxIdx
  
-print(find_max([1, 23, 29, 12, 39, 7, 15]))
+# 2-1
+def c2_1(numList):
+    min = numList[0]
+    for i in range(0, len(numList)):
+        if min > numList[i]:
+            min = numList[i]
+    return min
+
+print(c2_0([1, 23, 29, 12, 39, 7, 15]))
+print(c2_0_1([1, 23, 29, 12, 39, 7, 15]))
+print(c2_1([1, 23, 29, 12, 39, 7, 15]))
