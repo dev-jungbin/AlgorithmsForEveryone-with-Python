7281372c9fe92c9f3e2766463ccbc4a1d56cd044
Jungbin Jung <lollin0@naver.com>
2021-11-09 19:43:58 +0900

    [CHORE] Practice chapter-6

--- chapter-4.py
+++ chapter-4.py
@@ -25,3 +25,21 @@ def c4_2(numList, count):
 
 numArray = [1, 2, 3, 4, 5, 7, 3, 5]
 print(c4_2(numArray, len(numArray)))
+
+
+
+def find_max(i, n):
+    if n == 1:
+        return i[0]
+
+    max_val = find_max(i, n-1)
+
+    if max_val > i[n-1]:
+        return max_val
+
+    else:
+        return i[n-1]
+
+
+v = [0, 1, 2, 3, 4, 5]
+print(find_max(v, len(v)))
