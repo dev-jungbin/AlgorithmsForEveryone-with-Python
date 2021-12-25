7b518927a24d0088d834d5b22f871e599a3fb5f8
Jungbin Jung <lollin0@naver.com>
2021-12-25 23:08:43 +0900

    Update chapter-9.py

--- chapter-9.py
+++ chapter-9.py
@@ -25,33 +25,35 @@
 # 9-0-2 삽입 정렬 알고리즘
 # a: 리스트
 
-# def ins_sort(a):
-#     n = len(a)
-#     for i in range(1, n):
-#         key = a[i]
-#         j = i - 1
-#         while j >= 0 and a[j] > key:
-#             a[j + 1] = a[j]
-#             j -= 1
-#         a[j + 1] = key
-
-# d = [2, 4, 5, 1, 3]
-# ins_sort(d)
-# print(d)
-
-# 9-2 삽입 정렬 알고리즘 - 내림차순
-# a: 리스트
-
-def reverse_ins_sort(a):
+def ins_sort(a):
     n = len(a)
     for i in range(1, n):
         key = a[i]
         j = i - 1
-        while j >= 0 and a[j] < key:
+        while j >= 0 and a[j] > key:
             a[j + 1] = a[j]
             j -= 1
+            print(a)
         a[j + 1] = key
-        
-d = [2, 4, 5, 1, 3, 1, 2, 1, 5, 6, 7, 2, 3, 0]
-reverse_ins_sort(d)
+        print(a)
+
+d = [2, 4, 5, 1, 3]
+ins_sort(d)
 print(d)
+
+# 9-2 삽입 정렬 알고리즘 - 내림차순
+# a: 리스트
+
+# def reverse_ins_sort(a):
+#     n = len(a)
+#     for i in range(1, n):
+#         key = a[i]
+#         j = i - 1
+#         while j >= 0 and a[j] < key:
+#             a[j + 1] = a[j]
+#             j -= 1
+#         a[j + 1] = key
+        
+# d = [2, 4, 5, 1, 3, 1, 2, 1, 5, 6, 7, 2, 3, 0]
+# reverse_ins_sort(d)
+# print(d)
